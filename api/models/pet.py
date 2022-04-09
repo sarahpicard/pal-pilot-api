c97e0fb20135f4234a824e4060792644500b13a4
sarahpicard <sarahannrey225@gmail.com>
2022-04-09 15:04:24 -0400

    stub up basic pet model

---
+++ api/models/pet.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from api.models.db import db
+
+class Pet(db.Model):
+  __tablename__ = 'pets'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  breed = db.Column(db.String(100))
+  animal_type = db.Column(db.String(100))
+  weight = db.Column(db.String(100))
+  age = db.Column(db.Integer)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+  profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+
+  def __repr__(self):
+    return f"Pet('{self.id}', '{self.name}'"
+
+  def serialize(self):
+    pet = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    return pet
